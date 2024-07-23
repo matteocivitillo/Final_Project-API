4c3462c7f3c1d46ca42da021b4cbed9655de8f28
matteoCivitillo <matteo.civitillo@mail.polimi.it>
2024-07-23 18:13:13 +0200

    inizio creazione magazzino

--- magazzino/magazzino.c
+++ magazzino/magazzino.c
@@ -6,6 +6,10 @@
             Estrazione: L'elemento con la data di scadenza minima viene rimosso e viene applicato l'heapify down per mantenere la proprietà del min-heap.
         --> se la dimensione dell'array viene sforata bisogna fare una realloc e ricollocare tutto l'array con complessità O(N)
         --> non so come gestire le collisioni nell'hashmap
+
+        Dimensione della tabella: Scegli un numero primo maggiore del numero di elementi stimati (es. 10.007).
+        Funzione di hash: DJB2 o MurmurHash per bilanciare efficienza e bassa collisione.
+        Gestione delle collisioni: Usa liste concatenate o probing lineare/quadratico per gestire le collisioni.
     */
 
 #include <stdlib.h>
@@ -13,10 +17,27 @@
 #include <string.h>
 
 #define MAX_NAME_LEN 256
+#define INITIAL_CAPACITY
+
+//struct magazzino
+
+typedef struct nodo_heap {
+    int scadenza; 
+    int q;
+} Nodo_heap;
+
+typedef struct minHeap{
+    Nodo_heap *array;
+    int size;
+    int cap;
+} MinHeap;
+
+
+
 int istante=0;
 
 int main (){
-    FILE *fp= fopen("ricette.txt", "r");
+    FILE *fp= fopen("istruzioni.txt", "r");
     if(fp==NULL){
         printf("errore nell'apertura del file");
         return 1;
@@ -39,6 +60,7 @@ int main (){
 
         else if(strcmp(stringa, "rifornimento")==0){
             printf("rifornimento\n");
+            //completare questa parte 
         }
 
         istante++;
