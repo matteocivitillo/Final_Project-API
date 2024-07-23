9243ffd96cb8615b07d2f30a732a1f1f88d8a3f4
matteoCivitillo <matteo.civitillo@mail.polimi.it>
2024-07-23 13:30:14 +0200

    ricettario

--- ricettario.c
+++ ricettario.c
@@ -1,43 +1,3 @@
-/*
-passaggi da seguire:
-    1) ricezione della frequenza di ritiro ordini e della capacità del camion
-    2) ricezione e aggiunta delle ricette
-    3) ricezione e aggiunta scorte a magazzino
-    4) ricezione e gestione degli ordini
-    5) ritiro e consegna ordini
-*/
-
-/*
-    GESTIONE MAGAZZINO: 
-        combinazione di hashmap e heap:
-        creo hashmap che ha come chiave il nome dell'ingrediente e come valore un puntatore al min-heap che gesisce le quantità (vedi foglio).
-            Inserimento: L'elemento viene aggiunto alla fine dell'heap e viene applicato l'heapify up per mantenere la proprietà del min-heap.
-            Estrazione: L'elemento con la data di scadenza minima viene rimosso e viene applicato l'heapify down per mantenere la proprietà del min-heap.
-        --> se la dimensione dell'array viene sforata bisogna fare una realloc e ricollocare tutto l'array con complessità O(N)
-        --> non so come gestire le collisioni nell'hashmap
-
-    GESTIONE RICETTE: 
-        nell'ultimo test ci sono 1558 ricette quindi un hashmap con 2081 elementi potrebbe andare bene.
-        chiave dell'hashmap è il nome della ricetta e il valore è un altra mappa che associa ciascun ingrediente a un valore
-        ogni ricetta ha un counter che indica il numero di ordini arrivati per quella ricetta (sia ordini completati sia ordini in attesa)
-        il counter aumentato di 1 quando ricevo un ordine relativo a quella ricetta e va diminuito di 1 quando invio un ordine di quella ricetta
-        quando provo a rimuovere una ricetta devo verificare che il counter di essa sia a zero
-        una volta inseriti gli ingredienti calcolo anche il peso totale della ricetta
-
-    GESTIONE ORDINI: 
-        coda FIFO, orine piu recente viene elaborato e se eseguito viene messo in ordini eseguiti e i materiali vengono rimossi dal magazzino, può essere utile calcolare e salvare il peso dell'ordine.
-        se l'ordine non puo essere eseguito viene messo in un altra lista FIFO per gli ordini in attesa
-        --> quando eseguo un ordine devo anche eliminare tutti gli ingredienti gia scaduti
-        come verifico la fattibilità di un'orine senza prelevare gli elementi? cioe devo essere sicuro che l'ordine sia fattibile prima di prelevare elementi
-
-    FREQUENZA E CAPACITA' RITIRO:
-        sono le prima informazioni fornite, salvate in variabili globali perche tanto non verranno piu modificate
-
-    RITIRO E CONSEGNA ORDINI: 
-        scelgo ordini da consegnare in base all'ordine nella coda FIFO di ordini completati e li inserisco ordinati in base al peso in una lista ordini da spedire che poi stampo
-*/
-
-
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
@@ -73,10 +33,6 @@ void inserisci_ricetta(char*, Ingrediente*);
 void rimuovi_ricetta(char*);
 
 
-
-void ordine();
-void rifornimento();
-
 //variabili globali
 HashTable* ht;
 int frequenza_camion=0; 
@@ -94,7 +50,7 @@ int main (){
     fscanf(fp,"%d", &frequenza_camion);
     fscanf(fp,"%d", &capienza_camion);
     char stringa[100];
-    ht=crea_hashTable();
+    ht= crea_hashTable();
     //ciclo while che finche il file contiene stringhe continua a leggere
     while (fscanf(fp, "%s", &stringa)!= EOF){
 
@@ -128,7 +84,7 @@ int main (){
                 
             }
             inserisci_ricetta(nome_ricetta, primo); 
-            printf("aggiungi_ricetta %s\n", nome_ricetta);
+            printf("aggiunta ricetta: %s\n", nome_ricetta);
 
         }
 
@@ -235,10 +191,3 @@ void rimuovi_ricetta(char* nome){
         return;
     }    
 }
-
-     //aggiorna tempo corrente--> forse non conviene scartare subito i prodotti scaduti ma conviene 
-       //lasciarli dove sono e quando vengono incontrati in seguito si verifica se sono da scartare
-        /* 
-        chiama una delle seguenti funzioni e modifica le strutture dati:
-
-        */
